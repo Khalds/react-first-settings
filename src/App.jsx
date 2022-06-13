68a2b6c57ef93fa17bf8a80bceb22429066700d7
khald <abv.khallid@mail.ru>
2022-06-13 11:47:33 +0300

    first settings fors react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App(){
+  return (
+    <>
+    <header>
+      <h1>Task 1</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam repudiandae cumque ratione, quas ipsa ex eligendi quis veniam nobis ut consectetur recusandae debitis voluptatum at numquam, nihil hic, dolorem minima!</p>
+      <button>read more...</button>
+    </header>
+    </>
+  )
+}
+
+export default App
